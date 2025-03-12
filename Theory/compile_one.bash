0964284d37e1b65d48339205dffe4862a18ae6f0
Federico Conti <federicoconti1702@gmail.com>
2025-03-12 10:55:46 +0100

    Forensic Acquisition:     - Disk encryption: Bitlocker     - Toolset and examples

--- Theory/compile_one.bash
+++ Theory/compile_one.bash
@@ -1,4 +1,4 @@
 pandoc --output out/preview.pdf \
 -H styles/preamble.tex \
 --resource-path=.:media \
---verbose src/3.0.md 
+--verbose src/0.0.md src/ForensicAcquisition/3.0.md src/ForensicAcquisition/3.1.md 
