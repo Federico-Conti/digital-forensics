b88df4ac71e282236c4f96e935810228b41a132a
Federico Conti <federicoconti1702@gmail.com>
2025-04-05 10:33:15 +0200

    File System:     - fat     - ext

--- Theory/compile.bash
+++ Theory/compile.bash
@@ -2,7 +2,7 @@ pandoc --output out/result.pdf \
 -H styles/preamble.tex \
 --resource-path=.:media \
 --verbose src/0.0.md src/ForensicAcquisition/3.0.md src/ForensicAcquisition/3.1.md \
- src/FileSystem/4.0.md src/FileSystem/4.1.md
+ src/FileSystem/4.0.md src/FileSystem/4.1.md src/FileSystem/4.3.md
 
 
 
